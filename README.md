111c4fac945f1ea2bdf51a500e061ed3bb567fc5
sgforcert <sgforcert@gmail.com>
2024-10-10 18:32:11 -0700

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Text-Editor-Module19
+
 Text Editor Module19
+
+## Description
+
+A text editor that is setup as a PWA, deployed to Render, and installable on your device with offline capabilities. It also stores and retrieves data from an IndexedDB.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+- Use NPM to install [Express](https://www.npmjs.com/package/express)
+- Use NPM to install Dev dependencies [concurrently](https://www.npmjs.com/package/concurrently) and [nodemon](https://www.npmjs.com/package/nodemon)
+- Run npm install in command line to install dependencies
+- Run npm run start to start the app
+- Click install on the webpage to install the app for offline use
+
+## Usage
+
+- [Deployed Site] https://text-editor-module19.onrender.com
+
+## Tests
+
+No testing
+
+## Questions
+
+[GitHub profile]  
+https://github.com/sgforcert/Text-Editor-Module19
---
+++ client/src/js/database.js
@@ -0,0 +1,48 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: id, jate: content });
+    const result = await request;
+    console.log('Data Saved to the Database..');
+    return result;  
+  } catch (error) {
+    console.error('putDb not implemented');
+    console.error(error);
+  }
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('Data Saved to the Database..');
+    return result;
+    
+  } catch (error) {
+    console.error('getDb not implemented');
+    console.error(error);
+  }
+};
+
+initdb();
